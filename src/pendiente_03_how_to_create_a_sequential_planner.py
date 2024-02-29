368792713eefe573377f1f6c703908fedc3671b2
sebacastillo <castilloclaudiosebastian@gmail.com>
2024-02-29 06:36:44 -0300

    update

--- src/pendiente_03_how_to_create_a_sequential_planner.py
+++ src/pendiente_03_how_to_create_a_sequential_planner.py
@@ -1,18 +1,31 @@
 import asyncio
-
+import os 
+import sys
 import semantic_kernel as sk
-from semantic_kernel.connectors.ai.open_ai.services.open_ai_chat_completion import OpenAIChatCompletion
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion, AzureTextEmbedding
 from semantic_kernel.core_skills import TextSkill
 from semantic_kernel.planning import SequentialPlanner, Plan
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(root_dir)
 
-with open("./prompts/sk_seq_prompt", "r") as f:
+with open("./semantic_kernel_examples/examples/prompts/sk_seq_prompt", "r") as f:
     PROMPT = f.read()
 
 kernel = sk.Kernel()
 
-api_key, org_id = sk.openai_settings_from_dot_env()
-kernel.add_chat_service("gpt-3.5", OpenAIChatCompletion("gpt-3.5-turbo", api_key, org_id))
-skills_directory = "../skills/"
+api_key, org_id = sk.azure_aisearch_settings_from_dot_env()
+deployment_name, key, endpoint = sk.azure_openai_settings_from_dot_env()
+embeddings = os.environ["AZURE_OPENAI_EMBEDDINGS_MODEL_NAME"]
+
+azure_chat_service = AzureChatCompletion(deployment_name=deployment_name, endpoint=endpoint, api_key=key)
+azure_text_embedding = AzureTextEmbedding(deployment_name=embeddings, endpoint=endpoint, api_key=key)
+
+kernel.add_chat_service(
+        "chat_completion",
+        AzureChatCompletion(deployment_name=deployment_name, endpoint=endpoint, api_key=key),
+)
+
+skills_directory = "./semantic_kernel_examples/skills/"
 writer_skill = kernel.import_semantic_skill_from_directory(skills_directory, "WriterSkill")
 # writer_skill = kernel.import_semantic_skill_from_directory(skills_directory, "WriterSkill")
 summarize_skill = kernel.import_semantic_skill_from_directory(skills_directory, "SummarizeSkill")
